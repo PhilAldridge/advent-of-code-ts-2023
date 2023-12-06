773e693849c8b2b0877d43fc5bbd3eb90a588a12
phil <phil.aldridge@bjss.com>
2023-12-06 09:23:48 +0000

    Solve day 6

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -7,11 +7,28 @@ class Day6 extends Day {
     }
 
     solveForPartOne(input: string): string {
-        return input;
+        const sections = input.split('\r\n');
+    const times = sections[0].match(/\d+/g)?.map(str=> Number(str));
+    const distances = sections[1].match(/\d+/g)?.map(str=> Number(str));
+    console.log(distances)
+    let total = 1;
+    times?.forEach((time,index)=> {
+        let ways =0;
+        for(let i =1; i<time; i++){
+            if((time-i)*i>=(distances as number[])[index]) ways++
+        }
+        total *= ways
+    })
+        return total.toString();
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const sections = input.replaceAll(" ","").split('\r\n');
+        const time = sections[0].match(/\d+/g)?.map(str=> Number(str))[0] || 0;
+        const distance = sections[1].match(/\d+/g)?.map(str=> Number(str))[0] || 0;
+        const min = Math.ceil((time - Math.sqrt(time*time-4*distance))/2);
+        const max = Math.floor((time + Math.sqrt(time*time-4*distance))/2);
+        return (max-min+1).toString();
     }
 }
 
