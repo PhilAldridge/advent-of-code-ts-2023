0561c1ca4039f1e4ba7f4dbf58ea18568f6819ad
phil <phil.aldridge@bjss.com>
2023-12-08 12:55:09 +0000

    Solve 8

--- src/day8/index.ts
+++ src/day8/index.ts
@@ -9,18 +9,52 @@ class Day8 extends Day {
     solveForPartOne(input: string): string {
         const directions = getDirections(input);
         const mappings = getMappings(input)
-        return traverseMaps(directions,mappings,'AAA','ZZZ').toString();
+        return traverseMaps(directions,mappings).toString();
     }
 
     solveForPartTwo(input: string): string {
         const directions = getDirections(input);
         const mappings = getMappings(input);
-
-        return traverseMapsSimultaneously(directions,mappings).toString();
+        const starts = getEndsWithPositions(input, 'A');
+        const ends = getEndsWithPositions(input,'Z');
+        return traverseMapsSimultaneously(directions,mappings,starts,ends).toString();
     }
 }
 
 export default new Day8;
+
+function getLoops(directions:number[], mappings:[number,number][],ends:number[]): number[][] {
+    let loops: number[][] = [];
+    let mapsChecked: number[] = [];
+    ends.forEach(i=> {
+        for(let multiplier = 1; multiplier<1000; multiplier++){
+            let currentPosition =  i;
+            let currentLoop = [i];
+            mapsChecked.push(i);
+            let turns = 0;
+            while(turns<directions.length*multiplier-1) {
+                currentPosition = mappings[currentPosition][directions[turns%directions.length]];
+                currentLoop.push(currentPosition);
+                turns ++  
+            }
+            currentPosition = mappings[currentPosition][directions[turns%directions.length]];
+            if(currentPosition===i) {
+                loops.push(currentLoop);
+                break;
+            }
+        }
+
+    })
+    return loops;
+}
+
+function getLoopZs(loop:number[], ends:number[]): number[] {
+    let positions: number[] =[];
+    loop.forEach((pos,i)=> {
+        if(ends.includes(pos)) positions.push(i);
+    })
+    return positions;
+}
 
 function getDirections(input:string):number[] {
     let directions: number[] = [];
@@ -41,54 +75,86 @@ function getDirections(input:string):number[] {
     return directions;
 }
 
-function getMappings(input:string):mapping[]{
-    let mappings: mapping[] =[]
+function getMappings(input:string):[number,number][]{
+    let mappings: [number,number][] =[];
     const mapStr = input.split("\n").slice(2);
-
-    mapStr.forEach(str=>{
-        mappings.push({
-            name: str.substring(0,3),
-            0: str.substring(7,10),
-            1: str.substring(12,15)
-        }) 
-
+    const names = mapStr.map(str=> str.substring(0,3)).sort()
+    names.forEach(name=> {
+        const str = mapStr.find(str=>str.substring(0,3)===name);
+        if(!str) throw new Error("Lost"+str);
+        const ind1 = names.indexOf(str.substring(7,10));
+        const ind2 = names.indexOf(str.substring(12,15));
+        if(ind1*ind2<0) throw new Error( str.substring(7,10)+str.substring(12,15));
+        mappings.push([ind1, ind2])
     })
-
-
     return mappings;
 }
 
-function traverseMaps(directions:number[],mappings:mapping[], start:string, end:string): number{
+function getEndsWithPositions(input:string, end:string):number[] {
+    let positions: number[] = [];
+    const mapStr = input.split("\n").slice(2);
+    const names = mapStr.map(str=> str.substring(0,3)).sort();
+    names.forEach((name,i)=> {
+        if(name.endsWith(end)) positions.push(i)
+    })
+    return positions;
+}
+
+function traverseMaps(directions:number[],mappings:[number,number][]): number{
     let turns = 0;
-    let currentPosition = start;
-    while(currentPosition!==end) {
-        const map = mappings.find(map=>map.name===currentPosition);
-        if(!map) throw new Error("got lost");
-        currentPosition = map[directions[turns%directions.length]];
+    let currentPosition = 0;
+    while(currentPosition!==mappings.length-1) {
+        currentPosition = mappings[currentPosition][directions[turns%directions.length]];
         turns ++
     }
     return turns;
 }
 
-function traverseMapsSimultaneously(directions:number[], mappings: mapping[]): number {
+function traverseMapsSimultaneously(directions:number[], mappings: [number,number][], starts: number[], ends:number[]): number {
     let turns = 0;
-    let currentPositions = mappings.filter(map=>map.name.endsWith("A")).map(map=>map.name);
-    while(currentPositions.filter(name=>name.endsWith('Z')).length< 1/*currentPositions.length*/) {
-        let newPositions: string[] = [];
-        currentPositions.forEach(pos=> {
-            const map = mappings.find(map=>map.name===pos);
-            if(!map) throw new Error("got lost at " + pos);
-            newPositions.push(map[directions[turns%directions.length]]);
+    let currentPositions = starts;
+    console.log(starts)
+    const loops = getLoops(directions,mappings,ends);
+    const loopStarts = loops.map(loop=>loop[0]);
+    let loopsFound:loopFound[] = []
+    while(true){
+        let newPositions: number[] = [];
+        currentPositions.forEach(pos=>{
+            if(turns%directions.length===0 && loopStarts.includes(pos)){
+                const loop = loops.find(loop2=>loop2[0]===pos);
+                if(!loop) throw new Error("What???")
+                loopsFound.push({ 
+                    index:pos, 
+                    foundOnTurn:turns+1,
+                    length: loop.length,
+                    endPositions: getLoopZs(loop,ends)
+                })
+            } else {
+                newPositions.push(mappings[pos][directions[turns%directions.length]])
+            }
         })
+        turns++;
         currentPositions = newPositions;
-        
-        turns ++;
+        if(newPositions.length===0) {
+            turns = loopsFound.reduce((a,c)=>lcm(a,c.length),1)
+            break;
+        }
     }
-
+    
     return turns;
 }
 
-type mapping = {
-    name: string,
-    [key:number] : string
+type loopFound = {
+    index:number
+    foundOnTurn:number,
+    length:number,
+    endPositions:number[],
+}
+
+function gcd(a:number, b:number):number{
+    return a ? gcd(b % a, a) : b;
+}
+
+function lcm(a:number, b:number):number {
+    return a * b / gcd(a, b);
 }
