dbe02447fed118f8c65dace114eeedb264c923e3
phil <phil.aldridge@bjss.com>
2023-12-18 13:51:54 +0000

    solve 18

--- src/day18/index.ts
+++ src/day18/index.ts
@@ -8,76 +8,72 @@ class Day18 extends Day {
 
     solveForPartOne(input: string): string {
         const lines = input.split('\n');
-        let grid:string[]= new Array(1000).fill(' '.repeat(1000));
-        let position = [237,400];
-        const startPos = [position[0]+1,position[1]+1]
-        grid[position[0]]=grid[position[0]].substring(0,position[1]) + '#' + grid[position[0]].substring(position[1]+1)
-        
+        const commands:command[] = [];
         lines.forEach(line=>{
-            const command = line.split(' ');
-            const amount = Number(command[1])
-            switch(command[0]) {
-                case 'U':
-                    for(let i = 1;i<=amount; i++){
-                        grid[position[0]-i] = grid[position[0]-i].substring(0,position[1]) + '#'+ grid[position[0]-i].substring(position[1]+1);
-                    }
-                    position[0] -= amount;
-                    break;
-                case 'D':
-                    for(let i = 1;i<=amount; i++){
-                        grid[position[0]+i] = grid[position[0]+i].substring(0,position[1]) + '#'+ grid[position[0]+i].substring(position[1]+1);
-                    }
-                    position[0] += amount;
-                    break;
-                case 'L':
-                    grid[position[0]] = grid[position[0]].substring(0,position[1]-amount) + '#'.repeat(amount) +grid[position[0]].substring(position[1]);
-                    position[1] -= amount;
-                    break;
-                case 'R':
-                    grid[position[0]] = grid[position[0]].substring(0,position[1]+1) + '#'.repeat(amount) +grid[position[0]].substring(position[1]+1+amount);
-                    position[1] += amount;
-                    break;
-            }
-        });
-        //console.log(grid[0].length)
-        let total = 0
-        let placesToGrow = [startPos];
-        console.log(grid)
-        while(placesToGrow.length>0) {
-            let nextPlaces: [number,number][] = [];
-            placesToGrow.forEach(placeToGrow=>{
-                const result = growLake(placeToGrow[0],placeToGrow[1],grid)
-                if(result)
-                nextPlaces = nextPlaces.concat(result)
+            const split = line.split(' ');
+            commands.push({
+                direction: split[0],
+                amount: Number(split[1])
             })
-            placesToGrow = nextPlaces;
-        }
-        //console.log(grid)
-        grid.forEach(line=>{
-            total += (line.match(/#/g)||[]).length;
         })
+        const vertices = getVertices(commands);
+        let total = calcPolygonArea(vertices)
         //46334
         return total.toString();
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const lines = input.split('\n');
+        const commands:command[] = [];
+        const directions = ['R', 'D', 'L', 'U']
+        lines.forEach(line=>{
+            const split = line.split(' ')[2];
+            const directionDigit = split[split.length-2];
+            const directionChar = directions[Number(directionDigit)]
+            const amountHex = split.substring(2,split.length-2);
+            const amountDec = parseInt(amountHex,16);
+            commands.push({
+                direction: directionChar,
+                amount: amountDec
+            })
+        })
+        //console.log(commands)
+        const vertices = getVertices(commands);
+        let total = calcPolygonArea(vertices)
+        return total.toString();
     }
 }
 
-function growLake(lineI:number, colI:number, grid: string[]):[number,number][] |null {
-    if(grid[lineI][colI]==='#') return null;
-    let result:[number,number][]=[]
-    grid[lineI] = grid[lineI].substring(0,colI) + '#' + grid[lineI].substring(colI+1)
-    if(lineI!== 0 && grid[lineI-1][colI]!=='#') result.push([lineI-1,colI]);
-    if(lineI!== grid.length-1 && grid[lineI+1][colI]!=='#') result.push([lineI+1,colI]);
-    if(colI!==0 && grid[lineI][colI-1]!=='#') result.push([lineI,colI-1]);
-    if(colI!== grid[0].length-1 && grid[lineI][colI+1]!=='#') result.push([lineI,colI+1]);
-
-    return result;
+function getVertices(commands:command[]):vertex[] {
+    let position = {
+        x:0,
+        y:0
+    };
+    let vertices:vertex[] = [{x:0,y:0}];
+    commands.forEach(command=>{
+        switch(command.direction) {
+            case 'U':
+                position.y -= command.amount;
+                break;
+            case 'D':
+                position.y += command.amount;
+                break;
+            case 'L':
+                position.x -= command.amount;
+                break;
+            case 'R':
+                position.x += command.amount;
+                break;
+        }
+        vertices.push({
+            x:position.x,
+            y:position.y
+        })
+    });
+    return vertices;
 }
 
-function calcPolygonArea(vertices:vertex[]) {
+function calcPolygonArea(vertices:vertex[]):number {
     var total = 0;
 
     for (var i = 0, l = vertices.length; i < l; i++) {
@@ -88,9 +84,13 @@ function calcPolygonArea(vertices:vertex[]) {
 
       total += (addX * addY * 0.5);
       total -= (subX * subY * 0.5);
+      if(i!==0) {
+        if(vertices[i].x>vertices[i-1].x) total+= vertices[i].x-vertices[i-1].x;
+        if(vertices[i].y>vertices[i-1].y) total += vertices[i].y-vertices[i-1].y
+      }
     }
 
-    return Math.abs(total);
+    return Math.abs(total)+1;
 }
 
 type vertex = {
@@ -98,4 +98,8 @@ type vertex = {
     y:number
 }
 
+type command = {
+    direction: string
+    amount: number
+}
 export default new Day18;
