c0b077986f02b257cd5a41389daca2b1aeba85f5
phil <phil.aldridge@bjss.com>
2023-12-21 11:27:10 +0000

    solve 19

--- src/day19/index.ts
+++ src/day19/index.ts
@@ -1,4 +1,3 @@
-import { checkPrime } from "crypto";
 import { Day } from "../day";
 
 class Day19 extends Day {
@@ -22,7 +21,6 @@ class Day19 extends Day {
         this.addBuckets(sections[0]);
         while (accepted.container.length+rejected.container.length<numberOfPresents){
             this.runRules(sections[0]);
-            console.log(this.buckets)
         }
         let total =0
         accepted.container.forEach(present=>{
@@ -32,7 +30,38 @@ class Day19 extends Day {
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const sections = input.split('\n\n');
+        const accepted = {label: 'A', container:[]}
+        const rejected = {label: 'R', container:[]}
+        this.buckets=[];
+        this.buckets.push(accepted)
+        this.buckets.push(rejected)
+        const presents = [{
+                xlower:1, xupper:4000, 
+                mlower:1, mupper:4000, 
+                alower:1, aupper:4000, 
+                slower:1, supper:4000
+            }]
+        this.buckets.push({label:'in', container:presents});
+        this.addBuckets(sections[0]);
+        let leftToProcess = this.buckets
+                .filter(bucket=>bucket.label!=='A' && bucket.label!=='R')
+                .reduce((a,c)=>a + c.container.length,0)
+        while (leftToProcess>0){
+            this.runRules2(sections[0]);
+            leftToProcess = this.buckets
+                .filter(bucket=>bucket.label!=='A' && bucket.label!=='R')
+                .reduce((a,c)=>a + c.container.length,0)
+        }
+        let total =0
+        accepted.container.forEach(present=>{
+            console.log(present)
+            total += (present['xupper'] - present['xlower']+1)*
+                     (present['mupper'] - present['mlower']+1)*
+                     (present['aupper'] - present['alower']+1)*
+                     (present['supper'] - present['slower']+1);
+        })
+        return total.toString();
     }
 
     addBuckets = (section:string) => {
@@ -78,11 +107,59 @@ class Day19 extends Day {
             })
         })
     }
+
+    runRules2 = (section:string) => {
+        const lines = section.split('\n');
+        lines.forEach(line=>{
+            const fromStr = line.substring(0,line.indexOf('{'));
+            let from = this.buckets.find(bucket=>bucket.label === fromStr);
+            const ruleStrings = line.substring(line.indexOf('{')+1, line.indexOf('}')).split(',')
+            ruleStrings.forEach(ruleStr=>{
+                if(!from) throw new Error('what')
+                if(ruleStr.includes(':')) {
+                    const split = ruleStr.split(':');
+                    const to = this.buckets.find(bucket=>bucket.label===split[1]);
+                    if(!to) throw new Error ('Buck not found!' + split[1]);
+                    let newFromContainer:present[]=[];
+                    const letter = split[0][0];
+                    const symbol = split[0][1];
+                    const amount = Number(split[0].substring(2))
+                    from.container.forEach(present=>{
+                        let toCopy:present = JSON.parse(JSON.stringify(present))
+                        let fromCopy:present = JSON.parse(JSON.stringify(present))
+                        if(symbol==='>') {
+                            if(present[letter+'upper']>amount) {
+                                toCopy[letter+'lower'] = Math.max(amount+1,present[letter+'lower'])
+                                to.container.push(toCopy)
+                            }
+                            if(present[letter+'lower']<=amount) {
+                                fromCopy[letter+'upper'] = Math.min(amount, present[letter+'upper'])
+                                newFromContainer.push(fromCopy)
+                            }
+                        } else{
+                            if(present[letter+'upper']>=amount) {
+                                fromCopy[letter+'lower'] = Math.max(amount,present[letter+'lower'])
+                                newFromContainer.push(fromCopy)
+                            }
+                            if(present[letter+'lower']<amount) {
+                                toCopy[letter+'upper'] = Math.min(amount-1, present[letter+'upper'])
+                                to.container.push(toCopy)
+                            }
+                        }
+                    })
+                    from.container = newFromContainer;
+                } else {
+                    const to = this.buckets.find(bucket=>bucket.label===ruleStr);
+                    if(!to) throw new Error ('Buck not found' + ruleStr);
+                    to.container = [...to.container, ...from.container];
+                    from.container = [];
+                }
+            })
+        })
+    }
 }
 
 function checkMatch(present:present,rule:string):boolean {
-    const property = present[rule[0]];
-    console.log(property)
     if(rule[1]==='>') {
         return present[rule[0]] > Number(rule.substring(2))
     } else {
@@ -108,10 +185,6 @@ function getPresents(section:string):present[] {
 export default new Day19;
 
 type present = {
-    x:number
-    m:number
-    a:number
-    s:number
     [key:string]:number
 }
 
